63df4e2fcf85b8945bdf97afe0b87d42584ecdef
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-11 10:44:08 +0530

    updated comments

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,9 @@ const App = () => {
     <div>
         {
           /* Do not remove the main div */
+          // JSX inside the curly braces should be wrapped in a fragment 
+          // (<>...</> or <React.Fragment>...</React.Fragment>) to include both the <p> and <button> 
+          // elements as siblings inside the main <div>.
           <>
           <p>Button clicked {count} times</p>
           <button onClick={() => setCount(count+1)}>Click Me</button>
